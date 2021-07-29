a22bb0f82ea03f9ef46b7869e4c0c50070b027f1
lucassinggg <lucassinggg@gmail.com>
2021-07-29 10:35:04 -0300

    new basket and itembasket models

--- src/shop/models.py
+++ src/shop/models.py
@@ -3,17 +3,40 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Category(models.Model):
     name = models.CharField(blank=False, null=False, max_length=200)
 
 
 class Product(models.Model):
     name = models.CharField(blank=False, null=False, max_length=200)
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, related_name='products',
+                                on_delete=models.CASCADE)
     created = models.DateField(auto_now_add=True)
     modified = models.DateField(auto_now=True)
     image = models.URLField(null=False, blank=False)
     description = models.CharField(blank=False, null=False, max_length=2000)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.PositiveIntegerField()
+
+
+class Basket(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                primary_key=True)
+
+
+class ItemBasket(models.Model):
+    basket = models.ForeignKey(Basket,
+                                related_name='items',
+                                null=False,
+                                on_delete=models.CASCADE,
+                                primary_key=False)
+
+    product = models.ForeignKey(Product,
+                                related_name='itembaskets',
+                                null=False,
+                                on_delete=models.CASCADE,
+                                primary_key=False)
+    qty = models.PositiveIntegerField()
